d15455e9620d2701944a7af0fbbb8f34057fa8ba
JeremZed <j.thulliez@gmail.com>
2024-11-29 15:13:33 +0000

    wip feature type

--- package/src/bdf/feature_type.py
+++ package/src/bdf/feature_type.py
@@ -2,6 +2,51 @@ from urllib.parse import urlparse
 from dateutil import parser
 import os
 
+class FeatureExtraction:
+
+    def __init__(self, df):
+
+        self.list_of_chars = "abcdefghijklmnopqrstuvwxyz0123456789_"
+        self.chars_to_index = {}
+        self.index_to_chars = {}
+        self.vector_size = 50
+
+        self.df = df
+        self.columns = df.columns
+
+        for i, v in enumerate(self.list_of_chars):
+            self.chars_to_index[v] = i
+            self.index_to_chars[i] = v
+
+    def conv_to_vec(self, input):
+        """ Permet de convertir l'entrée en vecteur d'une dimension """
+        output = []
+        for c in input:
+            output.append(self.chars_to_index[c])
+
+        if len(output) >= self.vector_size:
+            return output[0:self.vector_size]
+        else:
+            return output + [0] * (self.vector_size - len(output))
+
+    @staticmethod
+    def run(series):
+
+        features = {
+            'is_null' : 0
+        }
+        col_name = series.name
+
+        if series.empty or series.isnull().all():
+            features['is_null'] = True
+
+            return features
+
+t = pd.DataFrame(data={"user_id" : [0,1,2]})
+f = FeatureExtraction(t)
+f.conv_to_vec('poiuytrezaqsdfghjklmnbvcxw')
+
+
 class FeatureTypeNumerical:
 
     tag = "numerical"
@@ -76,7 +121,7 @@ class FeatureTypePath:
     tag = "path"
 
     instances = [np.str_, str, np.object_]
-    extension_not_allow = ['com', 'fr', 'net', 'de']
+    extension_not_allow = []
 
     @staticmethod
     def check(series, mode=None):
@@ -84,27 +129,40 @@ class FeatureTypePath:
         if series.dtype in FeatureTypePath.instances:
             for x in series:
                 u = urlparse(f"{x}")
-                if (u.netloc == "" and u.scheme == "") and u.path != "" and FeatureTypePath.is_file_path(u.path) == False:
+                if (u.netloc != "" or u.scheme != "") or (u.path != "" and (FeatureTypePath.is_file_path(u.path) == False and FeatureTypePath.is_dir_path(u.path) == False)):
                     return False
             return True
 
         return False
+
     @staticmethod
     def is_file_path(p):
         """ Permet de vérifier si la chaine de caractères passée en paramètre correspond à un path de fichier """
-        # # SI aucun séparateur présent alors on considère qu'il ne s'agit pas d'un fichier
-        # if not any([ sep in p for sep in ['/', '\\'] ]):
-        #     return False
-        explode = p.split(".")
-        print(len(explode))
-        # SI une extension est présente dans le nom de base du path alors il s'agit d'un fichier
-        if len(explode) > 0:
-            last = explode[-1]
-            if last != "" and last not in FeatureTypePath.extension_not_allow:
-                return True
+
+        s = os.path.split(p)
+        #Si présence d'un point dans le positionnement du fichier en théorie dans le path alors il s'agit d'un path de chemin et non d'un path de dossier
+        if s[1] != "":
+            if "." in s[1]:
+                extension = s[1].split('.')
+                return extension[-1] not in FeatureTypePath.extension_not_allow
 
         return False
 
+    @staticmethod
+    def is_dir_path(p):
+        """ Permet de verifier si la chaine de caractères passée en paramètre correspond à un path de dossier sans verifier son existence """
+
+        #Si aucun séparateur de dossier n'est présent alors on ne cherche pas plus loin, il ne s'agit pas d'un path de dossier
+        if not any([ sep in p for sep in ['/', '\\'] ]):
+            return False
+
+        s = os.path.split(p)
+        #Si présence d'un point dans le positionnement du fichier en théorie dans le path alors il s'agit d'un path de chemin et non d'un path de dossier
+        if s[1] != "":
+            if "." in s[1]:
+                return False
+
+        return True
 
 class FeatureTypeDate:
     tag = "date"
@@ -123,6 +181,27 @@ class FeatureTypeDate:
                 #Si le cas n'est pas possible alors la feature n'est pas considéré comme numérique
                 return False
             return True
+        return False
+
+class FeatureTypeId:
+    tag = "ID"
+    instances = []
+
+    @staticmethod
+    def check(series, mode=None):
+        """ Permet de lancer le contrôle du type ID sur toute la série passée en paramètre """
+        # Si le nom de la feature comporte le terme ID alors on tente de confirmer qu'il s'agisse bien d'une feature de type ID
+        if '_id' in series.name.lower():
+            return True
+
+        # nu = series.nunique()
+        # n = len(series)
+        # ratio = nu * 100 / n
+        # # On par du principe que si plus de 98% des valeurs sont différentes au sein de la serie alors il s'agit d'un ID
+        # print(series.name, ratio)
+        # if ratio > 98.0:
+        #     return True
+
         return False
 
 class FeatureTypeMixed:
@@ -145,7 +224,8 @@ class FeatureType:
         FeatureTypeBoolean,
         FeatureTypeURL,
         FeatureTypeDate,
-        FeatureTypePath
+        FeatureTypePath,
+        FeatureTypeId,
     ]
 
     def __init__(self, df, mode=None):
@@ -168,3 +248,26 @@ class FeatureType:
             features[c] = tags
 
         return features
+
+
+aa = {
+    "user_id" : [0,1,2,3,4,5,6],
+    "a-a" : ["0.2",1,2,"3",4,5,6.0],
+    "b" : [0,1,'0',True,0,"1",0],
+    "c" : [True, False, "True", False, True, "False", True],
+    "d" : ['2012-12-13 00:00:00', '2012-12-14 00:00:00', '2012-12-15 00:00:00', '2012-12-16 00:00:00', '2012-12-17 00:00:00', '2012-12-18 00:00:00', '2012-12-19 00:00:00'],
+    "e" : ['/home/p.csv', '/home/p.csv', '/home/p.csv', '/home/p.csv', '/home/p.csv', '/home/p.csv', '/home/p.csv'],
+    "f" : ['p.csv', 'p.csv', 'p.csv', 'p.csv', 'p.csv', 'p.csv', 'p.csv'],
+    "gidout" : ['kjdfk/llksd/', 'kjdfk/llksd/','kjdfk/llksd/','kjdfk/llksd/','kjdfk/llksd/','kjdfk/llksd/','kjdfk/llksd/'],
+}
+df_test = pd.DataFrame(aa)
+
+# # print(df.columns)
+# all_types = {}
+# for c in df_test.columns:
+#     all_types[c] = FeatureType.check(df_test[c])
+
+# all_types
+
+ft = FeatureType(df, mode=FeatureType.mode_convert)
+ft.check_all()
